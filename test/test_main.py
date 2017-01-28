828d55072a79c7b382e717717991bd3b6e3ab794
Brian Schlenker <bschlenk@umich.edu>
2017-01-28 12:28:27 -0800

    Add test stub

---
+++ test/test_main.py
@@ -0,0 +1,8 @@
+# TODO: test some stuff
+
+
+
+expected_bulbasaur = {'stardust': 2500, 'name': 'Bulbasaur', 'cp': 600, 'hp': 62}
+
+
+# TODO automatically associate the expected_{name} with the corresponding screenshot file
